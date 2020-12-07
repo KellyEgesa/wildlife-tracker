fec72d063bd89baec2cb78dcd4c165cb91835cc9
KellyEgesa <kelly.egesa@gmail.com>
2020-12-07 10:59:46 +0300

    added tests to sightings and passed them

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,5 +1,32 @@
 public class Sightings {
-    public Sightings(int rangersId, int locationId, int animalId) {
+    private int rangersId;
+    private int locationId;
+    private int animalId;
+    private int id;
 
+    public Sightings(int rangersId, int locationId, int animalId) {
+        this.rangersId = rangersId;
+        this.locationId = locationId;
+        this.animalId = animalId;
+    }
+
+    public int getRangersId() {
+        return rangersId;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -6,7 +6,11 @@ public class SightingsTest {
     private Sightings setUpSightings(){
         Animal testAnimal =  new Animal("Honey Badger", "Young");
         Rangers testRanger =  new Rangers("kelly.egesa@gmail.com", 158920);
-        return new Sightings(1,1,1);
+        Location testLocation = new Location("Near the River");
+        testAnimal.setId(1);
+        testRanger.setId(1);
+        testLocation.setId(1);
+        return new Sightings(testRanger.getId(),testLocation.getId(),testAnimal.getId());
     }
 
     @Test
@@ -15,5 +19,28 @@ public class SightingsTest {
         assertTrue(testSighting instanceof Sightings);
     }
 
+    @Test
+    public void getLocationIdCorrectly_1() {
+        Sightings testSighting = setUpSightings();
+        assertEquals(1, testSighting.getLocationId());
+    }
 
+    @Test
+    public void getRangerIdCorrectly_1() {
+        Sightings testSighting = setUpSightings();
+        assertEquals(1, testSighting.getRangersId());
+    }
+
+    @Test
+    public void getAnimalIdCorrectly_1() {
+        Sightings testSighting = setUpSightings();
+        assertEquals(1, testSighting.getAnimalId());
+    }
+
+    @Test
+    public void setIdSetsIdCorrectly_1() {
+        Sightings testSighting = setUpSightings();
+        testSighting.setId(1);
+        assertEquals(1, testSighting.getId());
+    }
 }
